efcfc1c073e42561f6645bcaf1fc925e5d3e66e3
Humbat Jamalov <143912059+Camalzadeh@users.noreply.github.com>
2026-01-10 22:46:09 +0400

    finished Ai part , added A bit Android configuration

--- composeApp/src/commonMain/kotlin/org/signa/app/data/service/AiService.kt
+++ composeApp/src/commonMain/kotlin/org/signa/app/data/service/AiService.kt
@@ -1,8 +1,8 @@
-package org.signa.app.domain.service
+package org.signa.app.data.service
 
 import org.signa.app.domain.model.Signal
-import org.signa.app.domain.util.Result
-import org.signa.app.domain.util.DataError
+import org.signa.app.domain.core.Result
+import org.signa.app.domain.core.DataError
 
 interface AiService {
     suspend fun analyzeSignals(signals: List<Signal>): Result<String, DataError>
